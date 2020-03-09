1692c72832502a4a4b33ef86740b6bebbf342c3a
xyangx0205 <xyangx0205@gmail.com>
2020-03-09 16:42:55 +0800

    practice debug

---
+++ lab7_adv_debug.js
@@ -0,0 +1,13 @@
+var http = require('http')
+function processRecord(request, response) {
+    var body = 'Node.JS Tutorial\n'
+    var contentLength = body.length
+    response.writeHead(200, {
+        'Content-Length': contentLength,
+        'Content-Type': 'text/plain'
+    })
+    response.end(body)
+}
+var server = http.createServer(processRecord)
+console.log("server created, start listen to port 8765")
+server.listen(8765)
